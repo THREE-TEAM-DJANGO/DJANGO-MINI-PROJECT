041f70b31ef7829f6a9f91f5d7c48044c0587236
enjore <shawn630730@gmail.com>
2025-06-09 09:01:26 +0700

    setting.py 수정

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import json
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+with open(BASE_DIR / '.config_secret' / 'secret.json') as f:
+    config_secret_str = f.read()
 
+SECRET = json.loads(config_secret_str)
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&pp-a1hjez&(bh49vk6t((#$3e8$1nxjh0$81%y=-781c&ruq5"
+SECRET_KEY = SECRET['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
